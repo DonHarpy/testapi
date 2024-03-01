0609bcf38aac57c81987e09529e26fd7a505e1a9
Mustafa Harpy <mustafaharpy10@gmail.com>
2024-03-01 14:38:56 +0200

    last commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/SHA-testdatabase')
+const mongooseServer = 'mongodb://127.0.0.1:27017/SHA-testdatabase'
+mongoose.connect(mongooseServer)
 
 
 // const firstUser = new Users({
